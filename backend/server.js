1dcccec308fc40aafb4a6a0c2d6cb79c41b2e1c3
gohilviishal <viishalgohil193@gmail.com>
2024-03-17 14:24:22 +0530

    chatglory app setup

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const app = express();
+
+app.use(express.json())
+const PORT = process.env.PORT || 5000;
+
+app.use((req,res,next)=>{
+    console.log(req.path,req.method)
+    next();
+})
+
+mongoose.connect(process.env.URI).then(() => {
+  app.listen(PORT, () => {
+    console.log(`server is listening on port ${PORT}`);
+  });
+});
